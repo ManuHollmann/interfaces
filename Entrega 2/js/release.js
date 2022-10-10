c694c644a189e44ff94b766ad59405f08fdcdba2
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2022-10-10 16:25:15 -0300

    Refactor codigo

--- Entrega 2/js/release.js
+++ Entrega 2/js/release.js
@@ -5,6 +5,21 @@ setTimeout(function () {
 
 const slider = document.querySelectorAll(".slider-inner");
 const progressBar = document.querySelectorAll(".prog-bar-inner");
+let number = document.querySelector(".number");
+
+let num = 0;
+let counter = setInterval(countLoad, 50, num);
+
+function countLoad() {
+  if (num === 100) {
+    clearInterval(counter);
+    document.querySelector(".circular").style.display = "none";
+    document.querySelector(".container").style.display = "block";
+  } else {
+    num++;
+    number.innerHTML = num + "<p>%</p>";
+  }
+}
 
 let sliderGrabbed = false;
 
