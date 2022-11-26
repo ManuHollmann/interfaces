d579314329982c02643b33909a686b3ed39e4ae9
ManuHollmann <j.mhollmann@hotmail.com>
2022-11-25 22:08:57 -0300

    Se implemento keyframe para animar el menu hamburguesa

--- Entrega 4/js/hamburger.js
+++ Entrega 4/js/hamburger.js
@@ -1,17 +1,12 @@
 /*/menu hamburguesa/;*/
 
-let line1 = document.querySelector(".line1__bars-menu");
-let line2 = document.querySelector(".line2__bars-menu");
-let line3 = document.querySelector(".line3__bars-menu");
-document.querySelector(".bars__menu").addEventListener("click", animateMenu);
+let menu = document.getElementById("hamburger").addEventListener("click", animateMenu);
 document.querySelectorAll("menu__item--show").forEach((x) => {
   x.addEventListener();
 });
 
 function animateMenu() {
-  line1.classList.toggle("activeline1__bars-menu");
-  line2.classList.toggle("activeline2__bars-menu");
-  line3.classList.toggle("activeline3__bars-menu");
+  document.getElementById("hamburger").classList.toggle("active__bars-menu");
   let shows = document.querySelectorAll(".nav-show");
   shows.forEach((show) => {
     show.classList.toggle("active-container-menu");
